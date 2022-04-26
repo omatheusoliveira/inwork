920596d04121770db8d214b58676cd108e000cd4
Matheus Araujo <matheus-araujo2001@hotmail.com>
2022-04-26 11:03:19 -0300

    criado vaga item

---
+++ src/components/VagaItem.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="vaga-item-content">
+    <div class="card">
+      <div class="content">
+        <div class="header">
+          <div class="title">
+            <h1>Auxiliar de produção - PCD</h1>
+          </div>
+          <div class="created">
+            <p>23/04</p>
+          </div>
+        </div>
+        <div class="body">
+          <div class="wage">
+            <p>De R$ 1.001,00 a R$ 2.000,00</p>
+          </div>
+          <div class="description">
+            <p>
+              Organização da produção (limpeza, organização de ferramentas).
+              Ajuda com preenchimento de relatórios. Conferencia na produção.
+            </p>
+          </div>
+        </div>
+        <div class="footer">
+          <div class="company">
+            <p>Empresa: Marilan Alimentos S.A.</p>
+          </div>
+          <div class="contact">
+            <h2>Entrar em contato</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="content">
+        <div class="header">
+          <div class="title">
+            <h1>Auxiliar de produção - PCD</h1>
+          </div>
+          <div class="created">
+            <p>23/04</p>
+          </div>
+        </div>
+        <div class="body">
+          <div class="wage">
+            <p>De R$ 1.001,00 a R$ 2.000,00</p>
+          </div>
+          <div class="description">
+            <p>
+              Organização da produção (limpeza, organização de ferramentas).
+              Ajuda com preenchimento de relatórios. Conferencia na produção.
+            </p>
+          </div>
+        </div>
+        <div class="footer">
+          <div class="company">
+            <p>Empresa: Marilan Alimentos S.A.</p>
+          </div>
+          <div class="contact">
+            <h2>Entrar em contato</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VagaItem",
+};
+</script>
+
+<style lang="less" scoped>
+.vaga-item-content {
+  .card {
+    width: 100%;
+    height: 302px;
+    background: white;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 12px;
+  }
+  .card:first-child {
+    margin-bottom: 60px;
+  }
+
+  .content {
+    padding: 20px 24px 40px 24px;
+
+    .header {
+      display: flex;
+      text-decoration: underline;
+      align-items: center;
+      justify-content: space-between;
+
+      .title {
+        h1 {
+          margin: 0;
+          font-weight: 500;
+        }
+      }
+      .created {
+        p {
+          margin: 0;
+          font-size: 24px;
+          font-weight: 500;
+        }
+      }
+    }
+
+    .body {
+      width: 530px;
+
+      .wage {
+        margin: 17px 0;
+      }
+
+      .description {
+        font-weight: 500;
+        font-size: 18px;
+        margin: 17px 0 29px 0;
+      }
+    }
+
+    .footer {
+      display: flex;
+      justify-content: space-between;
+
+      .company {
+        p {
+          margin: 0;
+          font-size: 18px;
+        }
+      }
+      .contact {
+        h2 {
+          margin: 0;
+          text-decoration: underline;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/HomePF.vue
+++ src/views/HomePF.vue
@@ -1,21 +1,64 @@
 <template>
-    <div class="homepf">
-        <MenuLateral />
+  <div class="homepf">
+    <MenuLateral />
+    <div class="container">
+      <div class="header">
+        <h2>Vagas para Marília e região</h2>
+        <input type="text" placeholder="Pesquisar por vagas" />
+      </div>
+      <div class="content">
+          <VagaItem />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
-import MenuLateral from "../components/MenuLateral.vue"
+import MenuLateral from "../components/MenuLateral.vue";
+import VagaItem from "../components/VagaItem.vue";
 
 export default {
-    nome: "HomePF",
-    components: {
-        MenuLateral
-    }
-}
+  name: "HomePF",
+  components: {
+    MenuLateral,
+    VagaItem,
+  },
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.homepf {
+  display: flex;
 
+  .container {
+    padding: 94px 150px;
+    width: calc(100vw - 253px);
+
+
+    .header {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      margin-bottom: 65px;
+
+      h2 {
+        margin: 0;
+        font-weight: 500;
+      }
+
+      input {
+        width: 250px;
+        height: 50px;
+        border-radius: 10px;
+        border: 0.1px solid #80808055;
+        text-indent: 17px;
+      }
+      input::-webkit-input-placeholder {
+        color: #494949;
+        font-weight: 400;
+        font-size: 16px;
+      }
+    }
+  }
+}
 </style>
